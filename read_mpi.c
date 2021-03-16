ae58c9a1751d6bd37e1dd5e0ad652379707129f5
Steven Zhou <venturizhou@gmail.com>
2021-03-16 10:56:45 -0400

    manual testing of read_mpi code 139

--- read_mpi.c
+++ read_mpi.c
@@ -48,87 +48,87 @@ int main(int argc, char *argv[])
         buffer = (double *)malloc(sizeof(double) * ncols);
         ans = (double *)malloc(sizeof(double) * ncols);
         master = 0;
-        if (myid == master)
-        {
-            // Master Code goes here
-            // aa = gen_matrix(nrows, ncols);
-            // b = gen_matrix(nrows, ncols);
-            starttime = MPI_Wtime();
-            numsent = 0;
-            MPI_Bcast(b, ntotal, MPI_DOUBLE, master, MPI_COMM_WORLD);
-            for (i = 0; i < min(numprocs - 1, nrows); i++)
-            {
-                for (j = 0; j < ncols; j++)
-                {
-                    buffer[j] = aa[i * ncols + j];
-                }
-                MPI_Send(buffer, ncols, MPI_DOUBLE, i + 1, i + 1, MPI_COMM_WORLD);
-                numsent++;
-            }
-            for (i = 0; i < nrows; i++)
-            {
-                MPI_Recv(ans, ncols, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG,
-                         MPI_COMM_WORLD, &status);
-                sender = status.MPI_SOURCE;
-                anstype = status.MPI_TAG;
-                for (int l = 0; l < ncols; l++)
-                {
-                    c[(anstype - 1) * ncols + l] = ans[l];
-                }
-                // c[anstype - 1] = ans;
-                if (numsent < nrows)
-                {
-                    for (j = 0; j < ncols; j++)
-                    {
-                        buffer[j] = aa[numsent * ncols + j];
-                    }
-                    MPI_Send(buffer, ncols, MPI_DOUBLE, sender, numsent + 1,
-                             MPI_COMM_WORLD);
-                    numsent++;
-                }
-                else
-                {
-                    MPI_Send(MPI_BOTTOM, 0, MPI_DOUBLE, sender, 0, MPI_COMM_WORLD);
-                }
-            }
-            endtime = MPI_Wtime();
-            printf("%f\n", (endtime - starttime));
-            // for(int t = 0; t < ntotal; t++){
-            //     printf("The %dth entry is %lf",t,c[t]);
-            // }
-        }
-        else
-        {
-            // Slave Code goes here
-            MPI_Bcast(b, ntotal, MPI_DOUBLE, master, MPI_COMM_WORLD);
-            if (myid <= nrows)
-            {
-                while (1)
-                {
-                    MPI_Recv(buffer, ncols, MPI_DOUBLE, master, MPI_ANY_TAG,
-                             MPI_COMM_WORLD, &status);
-                    if (status.MPI_TAG == 0)
-                    {
-                        break;
-                    }
-                    row = status.MPI_TAG;
-                    // #pragma omp parallel
-                    // #pragma omp shared(ans) for reduction(+:ans)
-                    for (j = 0; j < ncols; j++)
-                    {
-                        ans[j] = 0;
-                    }
-                    for (k = 0; k < ncols; k++)
-                    {
-                        for (j = 0; j < ncols; j++)
-                        {
-                            ans[j] += buffer[k] * b[k * ncols + j];
-                        }
-                    }
-                    MPI_Send(ans, ncols, MPI_DOUBLE, master, row, MPI_COMM_WORLD);
-                }
-            }
-        }
+        // if (myid == master)
+        // {
+        //     // Master Code goes here
+        //     // aa = gen_matrix(nrows, ncols);
+        //     // b = gen_matrix(nrows, ncols);
+        //     starttime = MPI_Wtime();
+        //     numsent = 0;
+        //     MPI_Bcast(b, ntotal, MPI_DOUBLE, master, MPI_COMM_WORLD);
+        //     for (i = 0; i < min(numprocs - 1, nrows); i++)
+        //     {
+        //         for (j = 0; j < ncols; j++)
+        //         {
+        //             buffer[j] = aa[i * ncols + j];
+        //         }
+        //         MPI_Send(buffer, ncols, MPI_DOUBLE, i + 1, i + 1, MPI_COMM_WORLD);
+        //         numsent++;
+        //     }
+        //     for (i = 0; i < nrows; i++)
+        //     {
+        //         MPI_Recv(ans, ncols, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG,
+        //                  MPI_COMM_WORLD, &status);
+        //         sender = status.MPI_SOURCE;
+        //         anstype = status.MPI_TAG;
+        //         for (int l = 0; l < ncols; l++)
+        //         {
+        //             c[(anstype - 1) * ncols + l] = ans[l];
+        //         }
+        //         // c[anstype - 1] = ans;
+        //         if (numsent < nrows)
+        //         {
+        //             for (j = 0; j < ncols; j++)
+        //             {
+        //                 buffer[j] = aa[numsent * ncols + j];
+        //             }
+        //             MPI_Send(buffer, ncols, MPI_DOUBLE, sender, numsent + 1,
+        //                      MPI_COMM_WORLD);
+        //             numsent++;
+        //         }
+        //         else
+        //         {
+        //             MPI_Send(MPI_BOTTOM, 0, MPI_DOUBLE, sender, 0, MPI_COMM_WORLD);
+        //         }
+        //     }
+        //     endtime = MPI_Wtime();
+        //     printf("%f\n", (endtime - starttime));
+        //     // for(int t = 0; t < ntotal; t++){
+        //     //     printf("The %dth entry is %lf",t,c[t]);
+        //     // }
+        // }
+        // else
+        // {
+        //     // Slave Code goes here
+        //     MPI_Bcast(b, ntotal, MPI_DOUBLE, master, MPI_COMM_WORLD);
+        //     if (myid <= nrows)
+        //     {
+        //         while (1)
+        //         {
+        //             MPI_Recv(buffer, ncols, MPI_DOUBLE, master, MPI_ANY_TAG,
+        //                      MPI_COMM_WORLD, &status);
+        //             if (status.MPI_TAG == 0)
+        //             {
+        //                 break;
+        //             }
+        //             row = status.MPI_TAG;
+        //             // #pragma omp parallel
+        //             // #pragma omp shared(ans) for reduction(+:ans)
+        //             for (j = 0; j < ncols; j++)
+        //             {
+        //                 ans[j] = 0;
+        //             }
+        //             for (k = 0; k < ncols; k++)
+        //             {
+        //                 for (j = 0; j < ncols; j++)
+        //                 {
+        //                     ans[j] += buffer[k] * b[k * ncols + j];
+        //                 }
+        //             }
+        //             MPI_Send(ans, ncols, MPI_DOUBLE, master, row, MPI_COMM_WORLD);
+        //         }
+        //     }
+        // }
     }
     else
     {
